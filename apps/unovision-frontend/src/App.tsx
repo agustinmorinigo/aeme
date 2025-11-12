a4b970691e97634df5178aae5c0f003e09c8edf7
Agustin Morinigo <agustinmorinigo1999@gmail.com>
2025-11-12 00:14:05 -0300

    test(monorepo): testing ui components

--- apps/unovision-frontend/src/App.tsx
+++ apps/unovision-frontend/src/App.tsx
@@ -1,38 +1,12 @@
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
 import './App.css';
-import { Card } from '@repo/ui/card';
-import TestFake from '@repo/ui/test-fake';
+import { Button } from '@repo/ui';
 
 function App() {
-  const [count, setCount] = useState<number>(1233322999);
-
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank' rel='noopener'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noopener'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card text-nuevo-8 bg-nuevo-3'>
-        <button type='button' onClick={() => setCount((count) => count + 1)}>
-          count is {count} TESTING COMMIT
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <TestFake />
-      <Card title='Card Title' href='feqji'>
-        This is a card component from the UI library.
-      </Card>
-      <p className='read-the-docs text-nuevo-8 bg-nuevo-3'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div>
+      <p className='text-nuevo-11 bg-amber-400 text-9xl'>HELLO WORLD!!</p>
+      <Button />
+    </div>
   );
 }
 
---
+++ packages/ui/src/components/button/button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button type='button' className='bg-red-500 p-4 text-nuevo-11 text-3xl'>
+      Esto es un button desde el paquete UI
+    </button>
+  );
+}
