754bb6c606a10717bab33b12acccf3281a36aaf6
Agustin Morinigo <agustinmorinigo1999@gmail.com>
2025-11-22 20:13:32 -0300

    feat(monorepo): ads

--- apps/unovision-backend/supabase/migrations/20251122201153_test_ci_cd_4.sql
+++ apps/unovision-backend/supabase/migrations/20251122201153_test_ci_cd_4.sql
@@ -1,2 +1,3 @@
 ALTER TABLE ONLY "public"."testing_ci_cd" ADD COLUMN "dir4" text DEFAULT 'valor_por_defecto_4';
 ALTER TABLE ONLY "public"."testing_ci_cd" ADD COLUMN "dir5" text DEFAULT 'valor_por_defecto_5';
+ALTER TABLE ONLY "public"."testing_ci_cd" ADD COLUMN "dir6" text DEFAULT 'valor_por_defecto_6';
