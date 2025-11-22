cac30fe69a725f3aecfa281eba3f29b93e264e9d
Agustin Morinigo <agustinmorinigo1999@gmail.com>
2025-11-22 16:07:47 -0300

    feat(unovision-backend): add new column dir4 to testing_ci_cd table

--- apps/unovision-backend/supabase/migrations/20251122181347_test_ci_cd_3.sql
+++ apps/unovision-backend/supabase/migrations/20251122181347_test_ci_cd_3.sql
@@ -1,2 +1,3 @@
 ALTER TABLE ONLY "public"."testing_ci_cd" ADD COLUMN "dir2" text DEFAULT 'valor_por_defecto_2';
 ALTER TABLE ONLY "public"."testing_ci_cd" ADD COLUMN "dir3" text DEFAULT 'valor_por_defecto_3';
+ALTER TABLE ONLY "public"."testing_ci_cd" ADD COLUMN "dir4" text DEFAULT 'valor_por_defecto_4';
