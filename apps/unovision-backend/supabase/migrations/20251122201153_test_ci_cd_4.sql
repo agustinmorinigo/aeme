4c43ecce3807d49e4d20ff662658ab90eda3cc07
Agustin Morinigo <agustinmorinigo1999@gmail.com>
2025-11-22 18:28:23 -0300

    fix(unovision-backend): test

--- apps/unovision-backend/supabase/migrations/20251122201153_test_ci_cd_4.sql
+++ apps/unovision-backend/supabase/migrations/20251122201153_test_ci_cd_4.sql
@@ -1,1 +1,2 @@
-ALTER TABLE ONLY "public"."testing_ci_cd" ADD COLUMN "dir4" text DEFAULT 'valor_por_defecto_3';
+ALTER TABLE ONLY "public"."testing_ci_cd" ADD COLUMN "dir4" text DEFAULT 'valor_por_defecto_4';
+ALTER TABLE ONLY "public"."testing_ci_cd" ADD COLUMN "dir5" text DEFAULT 'valor_por_defecto_5';
